97800c7bb05dab203b0105684da953166de46641
DesignisO <DesignIsO@protonmail.com>
2022-09-29 07:37:22 -0400

    adding files

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome to Flip-Tip")
+
+bill = float(input("What was your last bill total?"))
+
+tip = float(input("How much would you like to give in dollar amount please (0.00)? 10, 12, or 15?"))
+
+int(input("How many people to split the bill?"))
+
+bill_with_tip = tip/100 * bill
+
+print(bill_with_tip)
